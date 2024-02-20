2b6f57dd0fe8ebfdf00bcaf5f6e9d3ef41ba2841
rafael2347 <gonzalezmartinrafael1@gmail.com>
2024-02-20 01:16:20 +0100

    He podido hacer que si seleccionas una fecha, no se vuelva a tu fecha actual, sino se queda en donde lo has seleccionado, falta añadirle la base de datos firebase con los días festivos de granada o de España

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   DateTime today = DateTime.now();
-  
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +33,29 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget content() {
-    return Column(
-      children: [
-        const SizedBox(height: 20), // Agregué un SizedBox para separar los elementos
-        Container(
-          child: TableCalendar(
-            locale: "en_US",
-            rowHeight: 43,
-            headerStyle: HeaderStyle(formatButtonVisible: false, titleCentered: true),
-            availableGestures: AvailableGestures.all,
-            focusedDay: DateTime.now(),
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            onDaySelected: _onDaySelected,
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Text("Selected Day = " + today.toString().split(" ")[0]),
+          const SizedBox(height: 20), // Agregué un SizedBox para separar los elementos
+          Container(
+            child: TableCalendar(
+              locale: "en_US",
+              rowHeight: 43,
+              headerStyle:
+                  HeaderStyle(formatButtonVisible: false, titleCentered: true),
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              // Para poder seleccionar un día que no sea el día de hoy
+              focusedDay: today, // Establecer el día enfocado en la fecha seleccionada
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              onDaySelected: _onDaySelected,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
